4ac1af3fa4e97f0b16ef6a4c2af045e702332362
marcus <marcus.a.tracy@gmail.com>
2024-10-10 22:19:26 -0600

    Removed duplicate test

--- tests/register.spec.js
+++ tests/register.spec.js
@@ -26,30 +26,3 @@ test("Registration check (normal)", async ({ page }) => {
 	await page.getByPlaceholder("Password").fill("a");
 	await page.getByRole("button", { name: "Register" }).click();
 });
-
-test("Registration check (normal)", async ({ page }) => {
-	await page.route("*/**/api/auth", async (route) => {
-		const loginReq = { email: "d@jwt.com", password: "a" };
-		const loginRes = {
-			user: {
-				id: 3,
-				name: "Kai Chen",
-				email: "d@jwt.com",
-				roles: [{ role: "diner" }],
-			},
-			token: "abcdef",
-		};
-		expect(route.request().method()).toBe("POST");
-		expect(route.request().postDataJSON()).toMatchObject(loginReq);
-		await route.fulfill({ json: loginRes });
-	});
-
-	await page.goto("/"); // load the DOM router
-	await page.goto("/register");
-	// create account
-	await page.getByPlaceholder("Email address").click();
-	await page.getByPlaceholder("Email address").fill("d@jwt.com");
-	await page.getByPlaceholder("Email address").press("Tab");
-	await page.getByPlaceholder("Password").fill("a");
-	await page.getByRole("button", { name: "Register" }).click();
-});
